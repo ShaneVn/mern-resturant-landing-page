77597639b700a62f22924155787e8a2cb00cb4bc
Shane <shaneboo152@gmail.com>
2022-08-30 12:22:13 -0700

    fix a styling issue

--- frontend/src/components/TextInput/PaymentTextInput.jsx
+++ frontend/src/components/TextInput/PaymentTextInput.jsx
@@ -7,7 +7,7 @@ function PaymentTextInput({placeholder, setInPutValue ,email}) {
               type= { email ? "email" : "text"}
               placeholder={placeholder}
               className="app__bg  text-color_white
-               p-4  w-full font-openSans border-[1.5px] border-color_gray
+               p-4  w-full font-openSans border-[1.5px] border-color_gray outline-none
                 focus:border-color_golden rounded-[5px] "
               required
               onChange={(e)=>setInPutValue(e.target.value)}
